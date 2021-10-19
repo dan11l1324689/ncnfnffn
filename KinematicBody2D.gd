62a43893001d2e03bd9ae92a5765a924b66d16a8
dan11l1324689 <90840580+dan11l1324689@users.noreply.github.com>
2021-10-19 17:14:40 +0500

    Add files via upload

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -1,4 +1,10 @@
 extends KinematicBody2D
-
+var vert_move=0
 func _input(event):
 	print(event.as_text ())
+ 
+func _physics_process(delta):
+	var accel=Vector2()
+	accel.x+= 1 if Input.is_action_pressed ("RIGHT") else 0
+	accel.x+= 1 if Input.is_action_pressed ("LEFT") else 0
+	vert_move =-300 if Input .is_action_pressed("UP") else vert_move + 9.6*delta
